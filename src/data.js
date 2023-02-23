77033100dd11fcca71184181caf36d874ee2ab48
sobbey99 <sobbey99@gmail.com>
2023-02-23 23:33:44 +0200

    Slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://media.istockphoto.com/id/525973626/photo/retro-woman-in-floral-dress.jpg?b=1&s=170667a&w=0&k=20&c=M-MAMu7DrVZuZTtWsMyjyGXCUpQL4_shCeGE9gqiS58=",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://assets.ajio.com/medias/sys_master/root/20220729/tRaP/62e3ecb6aeb26921afac1279/-1117Wx1400H-462076842-yellow-MODEL.jpg",
+        title: "WINTER SALE",
+        desc: "STAY ON TOP FRESH",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-photo/smiling-beautiful-young-woman-pink-mini-dress-posing-studio_155003-14602.jpg",
+        title: "POPULAR SALE",
+        desc: "LOOK STRAIGHT FORWARD DON'T HESITATE",
+        bg: "fbf0f4",
+    }
+]
